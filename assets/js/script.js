72afbd33553629ef152cd0999074822d67f6318c
Charlotte Murphey <murphey.charlotte@gmail.com>
2022-10-13 14:50:11 -0400

    added recipe cards which load with recipe info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
 $(document).foundation();
+  
+const BREAKFASTURL ="https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast";
+const DESSERTURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert";
+const DINNERURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef";
+const LUNCHURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Side";
+const GINURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin";
+const RUMURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Rum";
+const VODKAURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka";
+const WHISKEYURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Whiskey";
+
+var breakfastLink = $("#breakfastRecipes");
+var lunchLink = $("#lunchRecipes");
+var dinnerLink = $("#dinnerRecipes");
+var dessertLink = $("#dessertRecipes");
+var ginLink = $("#ginRecipes");
+var vodkaLink = $("#vodkaRecipes");
+var whiskeyLink = $("#whiskeyRecipes");
+var rumLink = $("#rumRecipes");
+var repCards = $(".recipe-cards");
+var templateCard = $("#templateCard");
+var middlePage = $(".marketing-site-hero");
 
 
-const BREAKFASTURL ="https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast"
-const DESSERTURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert"
-const DINNERURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef"
-const LUNCHURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Side"
-const GINURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin"
-const RUMURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Rum"
-const VODKAURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka"
-const WHISKEYURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Whiskey"
-
+// javascript for added selectable cards to DOM which will hold recipes
+$('[data-cardSelectButton]').click(function() {
+    $(this).parent('[data-cardSelect]').toggleClass('is-selected');
+  });
 
 function drinktoggleMenu() {
     let categories = $("#drink-categories")
@@ -26,6 +42,35 @@ function toggleBreakfast(){
         return data.json()
     }).then(function(data){
         console.log(data)
+        //iterate through each returned object to retrieve a link to the full recipe
+        //based on the object ID number, then insert full link, recipe image and title
+        //into a selectable card created on the page
+        data.meals.forEach(m => {
+            var recipeID = m.idMeal;
+            var recipeTitle = m.strMeal;
+            var recipeImgLink = m.strMealThumb;
+            //create url to fetch recipe URL with recipe object ID number
+            var idSearchURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + recipeID;
+
+            fetch(idSearchURL)
+            .then(function(response){
+                return response.json()
+            }).then(function(data){
+                console.log(data)
+                //get link to actual recipe instructions
+                var recipeLink = data.meals[0].strSource;
+                var recipeCard = $(templateCard).clone();
+                $("img", recipeCard).attr("src", recipeImgLink)
+                $("h5", recipeCard).text(recipeTitle)
+                $("a", recipeCard).attr("href", recipeLink)
+                recipeCard.appendTo(repCards)
+                repCards.removeClass("hidden")
+                recipeCard.removeClass("hidden")
+                middlePage.addClass("hidden")
+                
+
+            })
+        })
     })
 }
 function toggleLunch(){
@@ -79,18 +124,6 @@ function toggleRum(){
     })
 }
 
-
-document.getElementById("categories-btn").addEventListener("click", toggleMenu)
-document.getElementById("drink-categories-btn").addEventListener("click", drinktoggleMenu)
-
-document.querySelector("#breakfast").addEventListener("click", toggleBreakfast)
-document.querySelector("#lunch").addEventListener("click", toggleLunch)
-document.querySelector("#dinner").addEventListener("click", toggleDinner)
-document.querySelector("#dessert").addEventListener("click", toggleDessert)
-
-document.querySelector("#rum").addEventListener("click", toggleRum)
-document.querySelector("#vodka").addEventListener("click", toggleVodka)
-document.querySelector("#whiskey").addEventListener("click", toggleWhiskey)
-document.querySelector("#gin").addEventListener("click", toggleGin)
+breakfastLink.on("click", toggleBreakfast);
 
 
